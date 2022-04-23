cedb67d4e25eb517346bd00437e6c5b93c6dadc6
nicugnm <nicolae.ghergu@s.unibuc.ro>
2022-04-23 17:43:21 +0300

    Add .idea files to gitignore and add constructors to AuthentificationDetailsUser

--- src/main/java/ro/coreblock/api/auth/models/AuthentificationDetailsUser.java
+++ src/main/java/ro/coreblock/api/auth/models/AuthentificationDetailsUser.java
@@ -1,10 +1,14 @@
 package ro.coreblock.api.auth.models;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.UUID;
 
+@NoArgsConstructor
+@AllArgsConstructor
 @Getter
 @Setter
 public class AuthentificationDetailsUser {
